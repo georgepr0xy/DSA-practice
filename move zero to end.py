719ee6d259adb3ecbc1c97bca8d8e75898a3fb5a
George Province <provincegeorge412@gmail.com>
2024-05-02 14:03:37 +0530

    move zeros to end

--- move zero to end.py
+++ move zero to end.py
@@ -18,4 +18,17 @@ for j in range(zero):
 	temp.append(0)
 arr = temp     
 print(arr)
+#-------------------optimal approach-------------->>
+
+j = -1
+for i in range(n):
+	if arr[i]==0:
+	    j = i
+	    break
+if j == -1:
+	return arr
+for i in range(j+1,n):
+	if arr[i] != 0:
+	    arr[i], arr[j]= arr[j], arr[i]
+	    j += 1
   
