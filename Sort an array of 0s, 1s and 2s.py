94e470d86a210f2e182d20840277365d84d47f4b
George Province <provincegeorge412@gmail.com>
2024-05-23 15:16:11 +0530

    same

--- Sort an array of 0s, 1s and 2s.py
+++ Sort an array of 0s, 1s and 2s.py
@@ -19,4 +19,25 @@ for i in range(b):
 for i in range(c):
    arr.append(2)   
 
-print(arr)        
+print(arr)     
+#--------------------->>.Dutch National flag algorithm. 
+
+
+arr = [2,0,1,0,2,0,2,1]
+n = len(arr)
+
+low = 0
+mid = 0
+high = n-1
+while mid <= high:
+   if arr[mid] == 0:
+      arr[low], arr[mid] = arr[mid], arr[low]
+      low += 1
+      mid += 1
+   elif arr[mid]==1:
+      mid += 1
+   else: 
+      arr[mid], arr[high] = arr[high], arr[mid]
+      high -= 1
+print(arr)      
+    
