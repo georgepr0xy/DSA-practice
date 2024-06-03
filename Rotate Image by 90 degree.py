3c28365e3169cd1fdb80fbb837541a23756204b5
George Province <provincegeorge412@gmail.com>
2024-06-03 10:03:06 +0530

    rotate matrix

---
+++ Rotate Image by 90 degree.py
@@ -0,0 +1,9 @@
+arr = [[1,2,3],[4,5,6],[7,8,9]]
+
+n = len(arr)
+rotated =[[0 for _ in range(n)] for _ in range(n)]
+print(rotated)
+for i in range(n):
+    for j in range(n):
+       rotated[j][n-i-1] = arr[i][j]
+print(rotated)       
