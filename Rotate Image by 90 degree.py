df8fb29c3de61132d37756a689f41bac1dd01bab
George Province <provincegeorge412@gmail.com>
2024-06-03 20:17:39 +0530

    spiral matrix

--- Rotate Image by 90 degree.py
+++ Rotate Image by 90 degree.py
@@ -1,9 +1,15 @@
 arr = [[1,2,3],[4,5,6],[7,8,9]]
 
 n = len(arr)
-rotated =[[0 for _ in range(n)] for _ in range(n)]
-print(rotated)
+# rotated =[[0 for _ in range(n)] for _ in range(n)]
+# print(rotated)
+# for i in range(n):
+#     for j in range(n):
+#        rotated[j][n-i-1] = arr[i][j]
+# print(rotated)      
+
+#-----------optimal--------------->
 for i in range(n):
-    for j in range(n):
-       rotated[j][n-i-1] = arr[i][j]
-print(rotated)       
+    for j in range(i):
+        arr[i][j], arr[j][i] = arr[j][i], arr[i][j]
+print(arr)        
