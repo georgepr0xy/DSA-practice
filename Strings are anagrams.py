d986a740516c6775120a6f59a40d8c42e107efd1
George Province <provincegeorge412@gmail.com>
2024-06-05 23:29:00 +0530

    anagram

---
+++ Strings are anagrams.py
@@ -0,0 +1,19 @@
+s1 = "abcdeabsd"
+s2 = "sfasdfasf"
+l1= list(s1)
+l2 = list(s2)
+l1.sort()
+l2.sort()
+print(l1)
+print(l2)
+n =len(l1)
+n = len(l2)
+yes = 0
+for i in range(n):
+    if l1[i]==l2[i]:
+        yes =1
+    if l1[i] != l2[i]:
+        yes = 0
+        break   
+if yes == 1:
+    print("anagram")        
