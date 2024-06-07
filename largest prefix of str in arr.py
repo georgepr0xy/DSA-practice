0b62e528d30517236a58ee517834fd73410a2132
George Province <provincegeorge412@gmail.com>
2024-06-07 14:22:24 +0530

    prefix in strings

---
+++ largest prefix of str in arr.py
@@ -0,0 +1,12 @@
+strings = ["flower", "flow", "flight"]
+# if not strings:
+#     print("no")
+# n = len(strings)
+# prefix = strings[0]
+# for i in strings[1:]:
+#     while not i.startswith(prefix):
+#         prefix = prefix[-1]
+#         if not prefix:
+#             print("no")
+# print(prefix)        
+
